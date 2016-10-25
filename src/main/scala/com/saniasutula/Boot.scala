378f145e1e290b5c8048b9dd6c553a3320c59aa4
sanchousese <saniasutula@gmail.com>
2016-10-25 14:37:28 +0300

    init

---
+++ src/main/scala/com/saniasutula/Boot.scala
@@ -0,0 +1,17 @@
+package com.saniasutula
+
+import akka.actor.{ActorSystem, Props}
+import akka.io.IO
+import spray.can.Http
+import akka.pattern.ask
+import akka.util.Timeout
+import scala.concurrent.duration._
+
+object Boot extends App {
+  implicit val system = ActorSystem("on-spray-can")
+
+  val service = system.actorOf(Props[MyServiceActor], "dataroot-service")
+
+  implicit val timeout = Timeout(5.seconds)
+  IO(Http) ? Http.Bind(service, interface = "localhost", port = 8080)
+}
---
+++ src/main/scala/com/saniasutula/MyService.scala
@@ -0,0 +1,42 @@
+package com.saniasutula
+
+import akka.actor.Actor
+import spray.routing._
+import spray.http._
+import MediaTypes._
+
+class MyServiceActor extends Actor with MyService {
+  def actorRefFactory = context
+
+  def receive = runRoute(myRoute)
+}
+
+trait MyService extends HttpService {
+
+  val myRoute =
+    path("") {
+      get {
+        respondWithMediaType(`text/html`) {
+          complete {
+            <html>
+              <body>
+                <h1>Say hello to <i>spray-routing</i> on <i>spray-can</i>!</h1>
+              </body>
+            </html>
+          }
+        }
+      }
+    } ~ path("hello") {
+      get {
+        respondWithMediaType(`text/html`) {
+          complete {
+            <html>
+              <body>
+                <h1>Gogi nogi!</h1>
+              </body>
+            </html>
+          }
+        }
+      }
+    }
+}
